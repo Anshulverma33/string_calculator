5502e44dabc715a12adb0977e3ea4a2bd842cd98
Anshul <anshulverma338@gmail.com>
2025-06-23 14:03:49 +0530

    feat: support new line delimiters

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,7 +1,7 @@
 int add(String numbers) {
   if (numbers.isEmpty) return 0;
 
-  final parts = numbers.split(',');
+  final parts = numbers.split(RegExp(r'[,\n]'));
   final nums = parts.map(int.parse).toList();
   return nums.reduce((a, b) => a + b);
 }
