2cd82aa39088f121f2800e6e3fc58532593891d8
Anshul <anshulverma338@gmail.com>
2025-06-23 14:04:48 +0530

    feat: handle custom delimiter syntax

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,7 +1,22 @@
 int add(String numbers) {
   if (numbers.isEmpty) return 0;
 
-  final parts = numbers.split(RegExp(r'[,\n]'));
-  final nums = parts.map(int.parse).toList();
-  return nums.reduce((a, b) => a + b);
+  String delimiterPattern = r'[,\n]';
+
+  if (numbers.startsWith('//')) {
+    final parts = numbers.split('\n');
+    final customDelimiter = parts[0].substring(2);
+    delimiterPattern = RegExp.escape(customDelimiter);
+    numbers = parts.sublist(1).join('\n');
+  }
+
+  final splitNumbers = numbers.split(RegExp(delimiterPattern));
+  final parsedNumbers = <int>[];
+
+  for (var n in splitNumbers) {
+    if (n.trim().isEmpty) continue;
+    parsedNumbers.add(int.parse(n));
+  }
+
+  return parsedNumbers.fold(0, (sum, n) => sum + n);
 }
