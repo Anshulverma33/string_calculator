03ca936a864aa664174db6e4b9290c394c464037
Anshul <anshulverma338@gmail.com>
2025-06-23 14:02:41 +0530

    feat: support multiple comma-separated numbers

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -1,7 +1,7 @@
 int add(String numbers) {
   if (numbers.isEmpty) return 0;
-  if (!numbers.contains(',') && !numbers.contains('\n')) {
-    return int.parse(numbers);
-  }
-  return 0;
+
+  final parts = numbers.split(',');
+  final nums = parts.map(int.parse).toList();
+  return nums.reduce((a, b) => a + b);
 }
